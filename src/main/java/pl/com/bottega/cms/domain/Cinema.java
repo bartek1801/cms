d99db08b439dadd840ddc53c726818d6870f5ad7
piotrslowinski <piotrslowinski89@gmail.com>
2017-12-16 16:43:35 +0100

    cinema class in domain package

---
+++ src/main/java/pl/com/bottega/cms/domain/Cinema.java
@@ -0,0 +1,11 @@
+package pl.com.bottega.cms.domain;
+
+/**
+ * Created by user on 16.12.2017.
+ */
+public class Cinema {
+
+    private Long id;
+    private String city;
+    private String name;
+}
