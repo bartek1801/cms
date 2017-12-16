172b42071c729c39605cbe69ad88966bcd705b3b
msysula <salavin84@tlen.pl>
2017-12-16 16:51:34 +0100

    create command

---
+++ src/main/java/pl/com/bottega/cms/domain/commands/CreateCinemaCommand.java
@@ -0,0 +1,22 @@
+package pl.com.bottega.cms.domain.commands;
+
+public class CreateCinemaCommand {
+
+    private String name, city;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
