008aa2b4869cb79129cacdd80d0be0a7b9e93560
piotrslowinski <piotrslowinski89@gmail.com>
2017-12-16 17:02:19 +0100

    repository in domain package

---
+++ src/main/java/pl/com/bottega/cms/domain/repositories/CinemaRepository.java
@@ -0,0 +1,13 @@
+package pl.com.bottega.cms.domain.repositories;
+
+import pl.com.bottega.cms.domain.Cinema;
+
+/**
+ * Created by user on 16.12.2017.
+ */
+public interface CinemaRepository {
+
+    void save(Cinema cinema);
+
+
+}
