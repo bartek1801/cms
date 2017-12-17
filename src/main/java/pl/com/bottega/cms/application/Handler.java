296ba41cc025fda81cf2053937a6091f82f0d55f
msysula <salavin84@tlen.pl>
2017-12-17 11:47:33 +0100

    handler, command

--- src/main/java/pl/com/bottega/cms/application/Handler.java
+++ src/main/java/pl/com/bottega/cms/application/Handler.java
@@ -1,5 +1,7 @@
 package pl.com.bottega.cms.application;
 
+import pl.com.bottega.cms.domain.commands.Command;
+
 public interface Handler<C extends Command> {
 
     void handle(C command);
---
+++ src/main/java/pl/com/bottega/cms/domain/commands/CreateMovieCommand.java
@@ -0,0 +1,4 @@
+package pl.com.bottega.cms.domain.commands;
+
+public class CreateMovieCommand implements Command {
+}
