644d2d568b340ff36f9565f7b02600a910820cec
Bartlomiej <lechbartlomiej1801@gmail.com>
2018-02-06 22:08:18 +0100

    Changes in TicketPrices

--- src/main/java/pl/com/bottega/cms/domain/TicketPrices.java
+++ src/main/java/pl/com/bottega/cms/domain/TicketPrices.java
@@ -10,7 +10,7 @@ import java.util.Map;
 @Embeddable
 public class TicketPrices {
 
-    public static final BigDecimal TICKET_DOES_NOT_EXIST = BigDecimal.valueOf(-1);
+    public static final BigDecimal TICKET_KIND_NOT_EXIST = BigDecimal.valueOf(-1);
 
     @ElementCollection
     private Map<String, BigDecimal> prices = new HashMap<>();
@@ -45,9 +45,7 @@ public class TicketPrices {
     public Receipt calculatePrice(CalculatePricesCommand command) {
         Receipt receipt = new Receipt();
         for (Ticket ticket : command.getTickets()) {
-            if (getTicketPrice(ticket.getKind()).equals(TICKET_DOES_NOT_EXIST))
-                continue;
-            else {
+            if (!getTicketPrice(ticket.getKind()).equals(TICKET_KIND_NOT_EXIST)){
                 receipt.addReceiptLine(ticket.getKind(), ticket.getCount(), getTicketPrice(ticket.getKind()));
             }
         }
@@ -58,7 +56,7 @@ public class TicketPrices {
     private BigDecimal getTicketPrice(String kind) {
         if (prices.containsKey(kind))
             return prices.get(kind);
-        return TICKET_DOES_NOT_EXIST;
+        return TICKET_KIND_NOT_EXIST;
     }
 
 
