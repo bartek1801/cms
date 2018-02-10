0b0f5ca5513c76f53376870becd43b18ad6d7d5d
piotrslowinski <piotrslowinski89@gmail.com>
2018-02-10 15:26:08 +0100

    JPACinemaRepository updated

--- src/main/java/pl/com/bottega/cms/domain/repositories/MovieRepository.java
+++ src/main/java/pl/com/bottega/cms/domain/repositories/MovieRepository.java
@@ -8,4 +8,5 @@ import pl.com.bottega.cms.domain.Movie;
 public interface MovieRepository extends  Repository<Movie> {
 
 
+
 }
--- src/main/java/pl/com/bottega/cms/infrastructure/JPAMovieRepository.java
+++ src/main/java/pl/com/bottega/cms/infrastructure/JPAMovieRepository.java
@@ -10,22 +10,22 @@ import javax.persistence.EntityManager;
 @Component
 public class JPAMovieRepository extends GenericJPARepository<Movie> implements MovieRepository {
 
-//    private EntityManager entityManager;
-//
-//    public JPAMovieRepository(EntityManager entityManager) {
-//        this.entityManager = entityManager;
-//    }
+    private EntityManager entityManager;
 
-//    @Override
-//    public void save(Movie movie) {
-//        entityManager.persist(movie);
-//    }
-//
-//    @Override
-//    public Movie get(Long movieId) {
-//        Movie movie = entityManager.find(Movie.class, movieId);
-//        if (movie == null)
-//            throw new NoSuchEntityException();
-//        return movie;
-//    }
+    public JPAMovieRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public void save(Movie movie) {
+        entityManager.persist(movie);
+    }
+
+    @Override
+    public Movie get(Long movieId) {
+        Movie movie = entityManager.find(Movie.class, movieId);
+        if (movie == null)
+            throw new NoSuchEntityException();
+        return movie;
+    }
 }
