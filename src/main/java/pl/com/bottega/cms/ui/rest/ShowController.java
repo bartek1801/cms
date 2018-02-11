f63317d22fe74738717ee8f49f82388c506b0909
msysula <salavin84@tlen.pl>
2018-02-11 11:11:00 +0100

    showController create

--- src/main/java/pl/com/bottega/cms/ui/rest/ShowController.java
+++ src/main/java/pl/com/bottega/cms/ui/rest/ShowController.java
@@ -5,15 +5,21 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.com.bottega.cms.application.CinemaHallDto;
+import pl.com.bottega.cms.infrastructure.JPACinemaFinder;
 
 @RestController
 @RequestMapping("/shows")
 public class ShowController {
 
+    private JPACinemaFinder finder;
+
+    public ShowController(JPACinemaFinder finder) {
+        this.finder = finder;
+    }
 
     @GetMapping("{showId}/seats/")
     CinemaHallDto getSeats(@PathVariable Long showId) {
-        return null;
+        return finder.getSeats(showId);
     }
 
 }
